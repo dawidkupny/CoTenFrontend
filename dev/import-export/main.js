143c898c7bc6917b1cac17a728755ccfda60a20f
Dawid <davvidkupny@gmail.com>
2021-01-30 15:22:48 +0100

    Tydzień 8 zakończona lekcja 4, webpack

---
+++ dev/import-export/main.js
@@ -0,0 +1,8 @@
+import {hello as hi, korsi} from './hello.js';
+import human from './maciek.js'
+import * as hello from './hello.js';
+
+console.log(hi('Maciek'));
+console.log(korsi);
+console.log(hello.korsi);
+console.log(human);
